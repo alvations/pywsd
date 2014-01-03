b39f6f46c1f188d17d62db7eb24119e35e281632
alvations <alvations@gmail.com>
2014-01-03 11:00:16 +0100

    added lesk.py

---
+++ lesk.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python -*- coding: utf-8 -*-
+
+from nltk.corpus import wordnet as wn
+from nltk.stem import PorterStemmer
+
+porter = PorterStemmer()
+
+def lesk(context_sentence, ambiguous_word, pos=None, stem=True, hyperhypo=True):
+  max_overlaps = 0; lesk_sense = None
+  context_sentence = context_sentence.split()
+  
+  for ss in wn.synsets(ambiguous_word):
+    # If POS is specified.
+    if pos and ss.pos is not pos:
+      continue
+    
+    lesk_dictionary = []
+    # Includes definition.
+    lesk_dictionary+= ss.definition.split()
+    # Includes lemma_names.
+    lesk_dictionary+= ss.lemma_names
+
+    # Optional: includes lemma_names of hypernyms and hyponyms.
+    if hyperhypo == True:
+      lesk_dictionary+= list(chain(*[i.lemma_names for i \
+                                     in ss.hypernyms()+ss.hyponyms()]))    
+    
+    # Matching exact words causes sparsity, so optional matching for stems.
+    if stem == True: 
+      lesk_dictionary = [porter.stem(i) for i in lesk_dictionary]
+      context_sentence = [porter.stem(i) for i in context_sentence] 
+    
+    overlaps = set(lesk_dictionary).intersection(context_sentence)
+    
+    if len(overlaps) > max_overlaps:
+      lesk_sense = ss
+      max_overlaps = len(overlaps)
+  return lesk_sense
