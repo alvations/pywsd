f564ab6f86392e68d6d2a0a3afbee1195fdb757e
alvations <alvations@gmail.com>
2014-01-03 11:26:51 +0100

    formatted test_wsd

--- baseline.py
+++ baseline.py
@@ -13,7 +13,12 @@ def first_sense(ambigous_word):
   return wn.synsets(ambiguous_word)[0]
 
 def max_lemma_count(ambiguous_word):
-  """ Returns the sense with the highest lemma_name count. """
+  """ 
+  Returns the sense with the highest lemma_name count. 
+  The max_lemma_count() can be treated as a rough gauge for the 
+  Most Frequent Sense (MFS), if no other sense annotated corpus is available.
+  NOTE: The lemma counts are from the Brown Corpus 
+  """
   sense2lemmacounts = {i:sum(j.count() for j in i.lemmas) \
                        for i in wn.synsets(ambiguous_word)}
   return max(sense2lemmacounts, key=sense2lemmacounts.get)
--- test_wsd.py
+++ test_wsd.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python -*- coding: utf-8 -*-
 
-from lesk import simple_lesk
-
 bank_sents = ['I went to the bank to deposit my money',
 'The river bank was full of dead fishes']
 
@@ -9,6 +7,7 @@ plant_sents = ['The workers at the industrial plant were overworked',
 'The plant was no longer bearing flowers']
 
 print "======== TESTING simple_lesk ===========\n"
+from lesk import simple_lesk
 print "Context:", bank_sents[0]
 answer = simple_lesk(bank_sents[0],'bank')
 print "Sense:", answer
@@ -23,6 +22,28 @@ print
 
 print "Context:", plant_sents[0]
 answer = simple_lesk(plant_sents[0],'plant','n', True)
+print "Sense:", answer
+print "Definition:",answer.definition
+print
+
+
+print "======== TESTING baseline ===========\n"
+from baseline import random_sense, first_sense
+from baseline import max_lemma_count as most_frequent_sense
+print "Context:", bank_sents[0]
+answer = random_sense('bank')
+print "Sense:", answer
+print "Definition:",answer.definition
+print
+
+print "Context:", bank_sents[0]
+answer = first_sense('bank')
+print "Sense:", answer
+print "Definition:",answer.definition
+print
+
+print "Context:", bank_sents[0]
+answer = most_frequent_sense('bank')
 print "Sense:", answer
 print "Definition:",answer.definition
 print
