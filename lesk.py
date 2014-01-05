59c87065dcf0a31266cadee8bf21809821fabf28
alvations <alvations@gmail.com>
2014-01-05 12:29:20 +0100

    added adapted_lesk() and original_lesk()

--- lesk.py
+++ lesk.py
@@ -6,35 +6,94 @@ from itertools import chain
 
 porter = PorterStemmer()
 
-def simple_lesk(context_sentence, ambiguous_word, \
-                pos=None, stem=True, hyperhypo=True):
+def compare_overlaps(context, synset_signature):
   max_overlaps = 0; lesk_sense = None
-  context_sentence = context_sentence.split()
-  
+  for ss in synset_signature:
+    overlaps = set(synset_signature[ss]).intersection(context)
+    if len(overlaps) > max_overlaps:
+      lesk_sense = ss
+      max_overlaps = len(overlaps)    
+  return lesk_sense
+
+def original_lesk (context_sentence, ambiguous_word, dictionary=None):
+  """
+  This function is the implementation of the original Lesk algorithm (1986).
+  It requires a dictionary which contains the definition of the different
+  sense of each word. See http://goo.gl/8TB15w
+  """
+  if not dictionary:
+    dictionary = {ss:ss.definition.split() for ss in wn.synsets(ambiguous_word)}
+  best_sense = compare_overlaps(context_sentence.split(), dictionary)
+  return best_sense    
+
+def simple_signature(ambiguous_word, pos=None, stem=True, hyperhypo=True):
+  """ 
+  Returns a synset_signature dictionary that includes signature words of a 
+  sense from its:
+  (i)   definition
+  (ii)  example sentences
+  (iii) hypernyms and hyponyms
+  """
+  synset_signature = {}
   for ss in wn.synsets(ambiguous_word):
     # If POS is specified.
     if pos and ss.pos is not pos:
       continue
-    
-    lesk_dictionary = []
+    signature = []
     # Includes definition.
-    lesk_dictionary+= ss.definition.split()
+    signature+= ss.definition.split()
+    # Includes examples
+    signature+= list(chain(*[i.split() for i in ss.examples]))
     # Includes lemma_names.
-    lesk_dictionary+= ss.lemma_names
-
+    signature+= ss.lemma_names
     # Optional: includes lemma_names of hypernyms and hyponyms.
     if hyperhypo == True:
-      lesk_dictionary+= list(chain(*[i.lemma_names for i \
-                                     in ss.hypernyms()+ss.hyponyms()]))    
-    
+      signature+= list(chain(*[i.lemma_names for i \
+                               in ss.hypernyms()+ss.hyponyms()]))    
     # Matching exact words causes sparsity, so optional matching for stems.
     if stem == True: 
-      lesk_dictionary = [porter.stem(i) for i in lesk_dictionary]
-      context_sentence = [porter.stem(i) for i in context_sentence] 
-    
-    overlaps = set(lesk_dictionary).intersection(context_sentence)
-    
-    if len(overlaps) > max_overlaps:
-      lesk_sense = ss
-      max_overlaps = len(overlaps)
-  return lesk_sense
+      signature = [porter.stem(i) for i in signature]
+    synset_signature[ss] = signature
+  return synset_signature
+
+def simple_lesk(context_sentence, ambiguous_word, \
+                pos=None, stem=True, hyperhypo=True):
+  """
+  Simple Lesk is somewhere in between using more than the 
+  original Lesk algorithm (1986) and using less signature 
+  words than adapted Lesk (Banerjee and Pederson, 2002)
+  """
+  # Get the signatures for each synset.
+  ss_sign = simple_signature(ambiguous_word, pos, stem, hyperhypo)
+  # Disambiguate the sense in context.
+  context_sentence = [porter.stem(i) for i in context_sentence.split()]
+  best_sense = compare_overlaps(context_sentence, ss_sign)  
+  return best_sense
+
+def adapted_lesk(context_sentence, ambiguous_word, \
+                pos=None, stem=True, hyperhypo=True):
+  """
+  This function is the implementation of the Adapted Lesk algorithm, 
+  described in Banerjee and Pederson (2002). It makes use of the lexical 
+  items from semantically related senses within the wordnet 
+  hierarchies and to generate more lexical items for each sense. 
+  see www.d.umn.edu/~tpederse/Pubs/cicling2002-b.pdf‎
+  """
+  # Get the signatures for each synset.
+  ss_sign = simple_signature(ambiguous_word, pos, stem, hyperhypo)
+  for ss in ss_sign:
+    related_senses = list(set(ss.member_holonyms() + ss.member_meronyms() + 
+                             ss.part_meronyms() + ss.part_holonyms() + 
+                             ss.similar_tos() + ss.substance_holonyms() + 
+                             ss.substance_meronyms()))
+    signature = list(chain(*[i.lemma_names for i in related_senses]))
+    # Matching exact words causes sparsity, so optional matching for stems.
+    if stem == True: 
+      signature = [porter.stem(i) for i in signature]
+    ss_sign[ss]+=signature
+  
+  # Disambiguate the sense in context.
+  context_sentence = [porter.stem(i) for i in context_sentence.split()]
+  best_sense = compare_overlaps(context_sentence, ss_sign)
+  return best_sense
+
--- test_wsd.py
+++ test_wsd.py
@@ -8,40 +8,46 @@ plant_sents = ['The workers at the industrial plant were overworked',
 
 print "======== TESTING simple_lesk ===========\n"
 from lesk import simple_lesk
+print "#TESTING simple_lesk() ..."
 print "Context:", bank_sents[0]
 answer = simple_lesk(bank_sents[0],'bank')
 print "Sense:", answer
 print "Definition:",answer.definition
 print
 
+print "#TESTING simple_lesk() with POS ..."
 print "Context:", bank_sents[1]
 answer = simple_lesk(bank_sents[1],'bank','n')
 print "Sense:", answer
 print "Definition:",answer.definition
 print
 
+print "#TESTING simple_lesk() with POS and stems ..."
 print "Context:", plant_sents[0]
 answer = simple_lesk(plant_sents[0],'plant','n', True)
 print "Sense:", answer
 print "Definition:",answer.definition
 print
 
-
 print "======== TESTING baseline ===========\n"
 from baseline import random_sense, first_sense
 from baseline import max_lemma_count as most_frequent_sense
+
+print "#TESTING random_sense() ..."
 print "Context:", bank_sents[0]
 answer = random_sense('bank')
 print "Sense:", answer
 print "Definition:",answer.definition
 print
 
+print "#TESTING first_sense() ..."
 print "Context:", bank_sents[0]
 answer = first_sense('bank')
 print "Sense:", answer
 print "Definition:",answer.definition
 print
 
+print "#TESTING most_frequent_sense() ..."
 print "Context:", bank_sents[0]
 answer = most_frequent_sense('bank')
 print "Sense:", answer
