f0b6041e9288c4160f79873c798c0b8c3a2898c3
alvas <alvations@gmail.com>
2014-06-05 17:19:07 +0200

    Using width 4 tabs as of PEP8

--- baseline.py
+++ baseline.py
@@ -5,23 +5,22 @@ import random
 random.seed(0)
 
 def random_sense(ambiguous_word):
-  """ Returns a ramdom sense. """
-  return random.choice(wn.synsets(ambiguous_word))
+    """ Returns a ramdom sense. """
+    return random.choice(wn.synsets(ambiguous_word))
 
 def first_sense(ambiguous_word):
-  """ Returns the first sense."""
-  return wn.synsets(ambiguous_word)[0]
+    """ Returns the first sense."""
+    return wn.synsets(ambiguous_word)[0]
 
 def max_lemma_count(ambiguous_word):
-  """ 
-  Returns the sense with the highest lemma_name count. 
-  The max_lemma_count() can be treated as a rough gauge for the 
-  Most Frequent Sense (MFS), if no other sense annotated corpus is available.
-  NOTE: The lemma counts are from the Brown Corpus 
-  """
-  try: sense2lemmacounts = {i:sum(j.count() for j in i.lemmas()) \
-                       for i in wn.synsets(ambiguous_word)}
-  except: sense2lemmacounts = {i:sum(j.count() for j in i.lemmas) \
-                       for i in wn.synsets(ambiguous_word)}
-  return max(sense2lemmacounts, key=sense2lemmacounts.get)
-  
+    """ 
+    Returns the sense with the highest lemma_name count. 
+    The max_lemma_count() can be treated as a rough gauge for the 
+    Most Frequent Sense (MFS), if no other sense annotated corpus is available.
+    NOTE: The lemma counts are from the Brown Corpus 
+    """
+    try: sense2lemmacounts = {i:sum(j.count() for j in i.lemmas()) \
+                              for i in wn.synsets(ambiguous_word)}
+    except: sense2lemmacounts = {i:sum(j.count() for j in i.lemmas) \
+                                 for i in wn.synsets(ambiguous_word)}
+    return max(sense2lemmacounts, key=sense2lemmacounts.get)
--- lesk.py
+++ lesk.py
@@ -32,209 +32,213 @@ def stem(word, option="wnlemma")
 '''
 
 def lemmatize(ambiguous_word):
-  """
-  Tries to convert a surface word into lemma, and if lemmatize word is not in
-  wordnet then try and convert surface word into its stem.
-  
-  This is to handle the case where users input a surface word as an ambiguous 
-  word and the surface word is a not a lemma.
-  """
-  lemma = wnl.lemmatize(ambiguous_word)
-  stem = porter.stem(ambiguous_word)
-  # Ensure that ambiguous word is a lemma.
-  if not wn.synsets(lemma):
-    if not wn.synsets(stem):
-      return ambiguous_word
+    """
+    Tries to convert a surface word into lemma, and if lemmatize word is not in
+    wordnet then try and convert surface word into its stem.
+    
+    This is to handle the case where users input a surface word as an ambiguous 
+    word and the surface word is a not a lemma.
+    """
+    lemma = wnl.lemmatize(ambiguous_word)
+    stem = porter.stem(ambiguous_word)
+    # Ensure that ambiguous word is a lemma.
+    if not wn.synsets(lemma):
+        if not wn.synsets(stem):
+            return ambiguous_word
+        else:
+            return stem
     else:
-      return stem
-  else:
-    return lemma
-     
+     return lemma
+
 
 def compare_overlaps_greedy(context, synsets_signatures):
-  """
-  Calculate overlaps between the context sentence and the synset_signature
-  and returns the synset with the highest overlap.
-  
-  Note: Greedy algorithm only keeps the best sense, see http://goo.gl/OWSfOZ
-  Keeping greedy algorithm for documentary sake because original_lesk is greedy.
-  """
-  max_overlaps = 0; lesk_sense = None
-  for ss in synsets_signatures:
-    overlaps = set(synsets_signatures[ss]).intersection(context)
-    if len(overlaps) > max_overlaps:
-      lesk_sense = ss
-      max_overlaps = len(overlaps)    
-  return lesk_sense
+    """
+    Calculate overlaps between the context sentence and the synset_signature
+    and returns the synset with the highest overlap.
+    
+    Note: Greedy algorithm only keeps the best sense, see http://goo.gl/OWSfOZ
+    
+    Only used by original_lesk(). Keeping greedy algorithm for documentary sake, 
+    because original_lesks is greedy.
+    """
+    max_overlaps = 0; lesk_sense = None
+    for ss in synsets_signatures:
+        overlaps = set(synsets_signatures[ss]).intersection(context)
+        if len(overlaps) > max_overlaps:
+            lesk_sense = ss
+            max_overlaps = len(overlaps)    
+    return lesk_sense
 
 def compare_overlaps(context, synsets_signatures, \
                      nbest=False, keepscore=False, normalizescore=False):
-  """ 
-  Calculates overlaps between the context sentence and the synset_signture
-  and returns a ranked list of synsets from highest overlap to lowest.
-  """
-  overlaplen_synsets = [] # a tuple of (len(overlap), synset).
-  for ss in synsets_signatures:
-    overlaps = set(synsets_signatures[ss]).intersection(context)
-    overlaplen_synsets.append((len(overlaps), ss))
-
-  # Rank synsets from highest to lowest overlap.
-  ranked_synsets = sorted(overlaplen_synsets, reverse=True)
-  
-  # Normalize scores such that it's between 0 to 1. 
-  if normalizescore:
-    total = float(sum(i[0] for i in ranked_synsets))
-    ranked_synsets = [(i/total,j) for i,j in ranked_synsets]
+    """ 
+    Calculates overlaps between the context sentence and the synset_signture
+    and returns a ranked list of synsets from highest overlap to lowest.
+    """
+    overlaplen_synsets = [] # a tuple of (len(overlap), synset).
+    for ss in synsets_signatures:
+        overlaps = set(synsets_signatures[ss]).intersection(context)
+        overlaplen_synsets.append((len(overlaps), ss))
     
-  if not keepscore: # Returns a list of ranked synsets without scores
-    ranked_synsets = [i[1] for i in sorted(overlaplen_synsets, reverse=True)]
+    # Rank synsets from highest to lowest overlap.
+    ranked_synsets = sorted(overlaplen_synsets, reverse=True)
     
-  if nbest: # Returns a ranked list of synsets.
-    return ranked_synsets
-  else: # Returns only the best sense.
-    return ranked_synsets[0]
+    # Normalize scores such that it's between 0 to 1. 
+    if normalizescore:
+        total = float(sum(i[0] for i in ranked_synsets))
+        ranked_synsets = [(i/total,j) for i,j in ranked_synsets]
+      
+    if not keepscore: # Returns a list of ranked synsets without scores
+        ranked_synsets = [i[1] for i in sorted(overlaplen_synsets, \
+                                               reverse=True)]
+      
+    if nbest: # Returns a ranked list of synsets.
+        return ranked_synsets
+    else: # Returns only the best sense.
+        return ranked_synsets[0]
 
 def original_lesk(context_sentence, ambiguous_word, dictionary=None):
-  """
-  This function is the implementation of the original Lesk algorithm (1986).
-  It requires a dictionary which contains the definition of the different
-  sense of each word. See http://goo.gl/8TB15wb
-  """
-  ambiguous_word = lemmatize(ambiguous_word)
-  if not dictionary:
-    dictionary = {ss:ss.definition.split() for ss in wn.synsets(ambiguous_word)}
-  best_sense = compare_overlaps_greedy(context_sentence.split(), dictionary)
-  return best_sense    
+    """
+    This function is the implementation of the original Lesk algorithm (1986).
+    It requires a dictionary which contains the definition of the different
+    sense of each word. See http://goo.gl/8TB15wb
+    """
+    ambiguous_word = lemmatize(ambiguous_word)
+    if not dictionary:
+        dictionary = {ss:ss.definition.split() for ss \
+                      in wn.synsets(ambiguous_word)}
+    best_sense = compare_overlaps_greedy(context_sentence.split(), dictionary)
+    return best_sense    
 
 def simple_signature(ambiguous_word, pos=None, stem=True, \
                      hyperhypo=True, stop=True):
-  """ 
-  Returns a synsets_signatures dictionary that includes signature words of a 
-  sense from its:
-  (i)   definition
-  (ii)  example sentences
-  (iii) hypernyms and hyponyms
-  """
-  synsets_signatures = {}
-  for ss in wn.synsets(ambiguous_word):
-    # If POS is specified.
-    try:
-      if pos and str(ss.pos()) is not pos:
-        continue
-    except:
-      if pos and str(ss.pos) is not pos:
-        continue
-    signature = []
-    # Includes definition.
-    try: signature+= ss.definition().split()
-    except: signature+= ss.definition.split()
-    # Includes examples
-    try: signature+= list(chain(*[i.split() for i in ss.examples()]))
-    except: signature+= list(chain(*[i.split() for i in ss.examples]))
-    # Includes lemma_names.
-    try: signature+= ss.lemma_names()
-    except: signature+= ss.lemma_names
-    # Optional: includes lemma_names of hypernyms and hyponyms.
-    if hyperhypo == True:
-      try: signature+= list(chain(*[i.lemma_names() for i \
-                               in ss.hypernyms()+ss.hyponyms()]))
-      except: signature+= list(chain(*[i.lemma_names for i \
-                               in ss.hypernyms()+ss.hyponyms()]))
-    # Optional: removes stopwords.
-    if stop == True:
-      signature = [i for i in signature if i not in stopwords.words('english')]    
-    # Matching exact words causes sparsity, so optional matching for stems.
-    if stem == True: 
-      signature = [porter.stem(i) for i in signature]
-    synsets_signatures[ss] = signature
-  return synsets_signatures
+    """ 
+    Returns a synsets_signatures dictionary that includes signature words of a 
+    sense from its:
+    (i)   definition
+    (ii)  example sentences
+    (iii) hypernyms and hyponyms
+    """
+    synsets_signatures = {}
+    for ss in wn.synsets(ambiguous_word):
+        # If POS is specified.
+        try:
+            if pos and str(ss.pos()) is not pos:
+                continue
+        except:
+            if pos and str(ss.pos) is not pos:
+                continue
+        signature = []
+        # Includes definition.
+        try: signature+= ss.definition().split()
+        except: signature+= ss.definition.split()
+        # Includes examples
+        try: signature+= list(chain(*[i.split() for i in ss.examples()]))
+        except: signature+= list(chain(*[i.split() for i in ss.examples]))
+        # Includes lemma_names.
+        try: signature+= ss.lemma_names()
+        except: signature+= ss.lemma_names
+        # Optional: includes lemma_names of hypernyms and hyponyms.
+        if hyperhypo == True:
+            try: signature+= list(chain(*[i.lemma_names() for i \
+                                          in ss.hypernyms()+ss.hyponyms()]))
+            except: signature+= list(chain(*[i.lemma_names for i \
+                                             in ss.hypernyms()+ss.hyponyms()]))
+        # Optional: removes stopwords.
+        if stop == True:
+            signature = [i for i in signature if i not in stopwords.words('english')]    
+        # Matching exact words causes sparsity, so optional matching for stems.
+        if stem == True: 
+            signature = [porter.stem(i) for i in signature]
+        synsets_signatures[ss] = signature
+    return synsets_signatures
 
 def simple_lesk(context_sentence, ambiguous_word, \
                 pos=None, stem=True, hyperhypo=True, \
                 nbest=False, keepscore=False, normalizescore=False):
-  """
-  Simple Lesk is somewhere in between using more than the 
-  original Lesk algorithm (1986) and using less signature 
-  words than adapted Lesk (Banerjee and Pederson, 2002)
-  """
-  # Ensure that ambiguous word is a lemma.
-  ambiguous_word = lemmatize(ambiguous_word) 
-  # Get the signatures for each synset.
-  ss_sign = simple_signature(ambiguous_word, pos, stem, hyperhypo)
-  # Disambiguate the sense in context.
-  context_sentence = [porter.stem(i) for i in context_sentence.split()]
-  best_sense = compare_overlaps(context_sentence, ss_sign, \
-                                nbest=nbest, keepscore=keepscore, \
-                                normalizescore=normalizescore)  
-  return best_sense
+    """
+    Simple Lesk is somewhere in between using more than the 
+    original Lesk algorithm (1986) and using less signature 
+    words than adapted Lesk (Banerjee and Pederson, 2002)
+    """
+    # Ensure that ambiguous word is a lemma.
+    ambiguous_word = lemmatize(ambiguous_word) 
+    # Get the signatures for each synset.
+    ss_sign = simple_signature(ambiguous_word, pos, stem, hyperhypo)
+    # Disambiguate the sense in context.
+    context_sentence = [porter.stem(i) for i in context_sentence.split()]
+    best_sense = compare_overlaps(context_sentence, ss_sign, \
+                                    nbest=nbest, keepscore=keepscore, \
+                                    normalizescore=normalizescore)  
+    return best_sense
 
 def adapted_lesk(context_sentence, ambiguous_word, \
                 pos=None, stem=True, hyperhypo=True, stop=True, \
                 nbest=False, keepscore=False, normalizescore=False):
-  """
-  This function is the implementation of the Adapted Lesk algorithm, 
-  described in Banerjee and Pederson (2002). It makes use of the lexical 
-  items from semantically related senses within the wordnet 
-  hierarchies and to generate more lexical items for each sense. 
-  see www.d.umn.edu/~tpederse/Pubs/cicling2002-b.pdf‎
-  """
-  # Ensure that ambiguous word is a lemma.
-  ambiguous_word = lemmatize(ambiguous_word)
-  # Get the signatures for each synset.
-  ss_sign = simple_signature(ambiguous_word, pos, stem, hyperhypo)
-  for ss in ss_sign:
-    related_senses = list(set(ss.member_holonyms() + ss.member_meronyms() + 
-                             ss.part_meronyms() + ss.part_holonyms() + 
-                             ss.similar_tos() + ss.substance_holonyms() + 
-                             ss.substance_meronyms()))
+    """
+    This function is the implementation of the Adapted Lesk algorithm, 
+    described in Banerjee and Pederson (2002). It makes use of the lexical 
+    items from semantically related senses within the wordnet 
+    hierarchies and to generate more lexical items for each sense. 
+    see www.d.umn.edu/~tpederse/Pubs/cicling2002-b.pdf‎
+    """
+    # Ensure that ambiguous word is a lemma.
+    ambiguous_word = lemmatize(ambiguous_word)
+    # Get the signatures for each synset.
+    ss_sign = simple_signature(ambiguous_word, pos, stem, hyperhypo)
+    for ss in ss_sign:
+        related_senses = list(set(ss.member_holonyms() + ss.member_meronyms() + 
+                                 ss.part_meronyms() + ss.part_holonyms() + 
+                                 ss.similar_tos() + ss.substance_holonyms() + 
+                                 ss.substance_meronyms()))
     
-    try:
-      signature = list([j for j in chain(*[i.lemma_names() for i in \
+        try:
+            signature = list([j for j in chain(*[i.lemma_names() for i in \
                       related_senses]) if j not in stopwords.words('english')])
-    except:
-      signature = list([j for j in chain(*[i.lemma_names for i in \
+        except:
+            signature = list([j for j in chain(*[i.lemma_names for i in \
                       related_senses]) if j not in stopwords.words('english')])
     # Matching exact words causes sparsity, so optional matching for stems.
     if stem == True: 
-      signature = [porter.stem(i) for i in signature]
+        signature = [porter.stem(i) for i in signature]
     ss_sign[ss]+=signature
   
-  # Disambiguate the sense in context.
-  context_sentence = [porter.stem(i) for i in context_sentence.split()]
-  best_sense = compare_overlaps(context_sentence, ss_sign, \
-                                nbest=nbest, keepscore=keepscore, \
-                                normalizescore=normalizescore)
-  return best_sense
+    # Disambiguate the sense in context.
+    context_sentence = [porter.stem(i) for i in context_sentence.split()]
+    best_sense = compare_overlaps(context_sentence, ss_sign, \
+                                    nbest=nbest, keepscore=keepscore, \
+                                    normalizescore=normalizescore)
+    return best_sense
 
 def cosine_lesk(context_sentence, ambiguous_word, stem=False, stop=True, \
                 nbest=False):
-  # Ensure that ambiguous word is a lemma.
-  ambiguous_word = lemmatize(ambiguous_word)
-  """ 
-  In line with vector space models, we can use cosine to calculate overlaps
-  instead of using raw overlap counts. Essentially, the idea of using 
-  signatures (aka 'sense paraphrases') is lesk-like.
-  """
-  from nltk.tokenize import word_tokenize
-  from cosine import cosine_similarity as cos_sim
-  synsets_signatures = simple_signature(ambiguous_word, stem=stem, stop=stop)
-  
-  scores = []
-  for ss, signature in synsets_signatures.items():
-    # Lowercase and replace "_" with spaces.
-    signature = " ".join(map(str, signature)).lower().replace("_", " ")
-    # Removes punctuation.
-    signature = [i for i in word_tokenize(signature) \
-                 if i not in string.punctuation]
-    # Optional: remove stopwords.
-    if stop:
-      signature = [i for i in signature if i not in stopwords.words('english')]
-    # Optional: stem the tokens.
-    if stem:
-      signature = [porter.stem(i) for i in signature]
-    scores.append((cos_sim(context_sentence, " ".join(signature)), ss))
-
-  if not nbest:
-    return sorted(scores, reverse=True)[0][1]
-  else:
-    return [(j,i) for i,j in sorted(scores, reverse=True)]
+    # Ensure that ambiguous word is a lemma.
+    ambiguous_word = lemmatize(ambiguous_word)
+    """ 
+    In line with vector space models, we can use cosine to calculate overlaps
+    instead of using raw overlap counts. Essentially, the idea of using 
+    signatures (aka 'sense paraphrases') is lesk-like.
+    """
+    from nltk.tokenize import word_tokenize
+    from cosine import cosine_similarity as cos_sim
+    synsets_signatures = simple_signature(ambiguous_word, stem=stem, stop=stop)
+    
+    scores = []
+    for ss, signature in synsets_signatures.items():
+        # Lowercase and replace "_" with spaces.
+        signature = " ".join(map(str, signature)).lower().replace("_", " ")
+        # Removes punctuation.
+        signature = [i for i in word_tokenize(signature) \
+                     if i not in string.punctuation]
+        # Optional: remove stopwords.
+        if stop:
+            signature = [i for i in signature if i not in stopwords.words('english')]
+        # Optional: stem the tokens.
+        if stem:
+            signature = [porter.stem(i) for i in signature]
+        scores.append((cos_sim(context_sentence, " ".join(signature)), ss))
+        
+        if not nbest:
+            return sorted(scores, reverse=True)[0][1]
+        else:
+            return [(j,i) for i,j in sorted(scores, reverse=True)]
