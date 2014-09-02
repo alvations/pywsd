bc375bc1ea5571c3035356e708e48d3cc6354182
alvas <alvations@gmail.com>
2014-09-02 11:08:37 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='pywsd',
+    version='0.1',
+    packages=['pywsd',],
+    long_description='Python Implementations of Word Sense Disambiguation (WSD) technologies',
+)
