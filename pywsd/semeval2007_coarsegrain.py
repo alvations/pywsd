d179d5890c125638cfb0226bce1b47ee01cf638d
alvas <alvations@gmail.com>
2014-09-22 11:52:56 +0200

    added beautifulsoup and semeval.py

--- pywsd/semeval2007_coarsegrain.py
+++ pywsd/semeval2007_coarsegrain.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python -*- coding: utf-8 -*-
 
-from collections import namedtuple
-
-from BeautifulSoup import BeautifulSoup as bsoup
-from utils import remove_tags
-
 coarse_all_words = """<?xml version="1.0" encoding="iso-8859-1" ?>
 <!DOCTYPE corpus SYSTEM "coarse-all-words.dtd">
 <corpus lang="en">
@@ -41207,41 +41202,4 @@ printf "\nTotal:\tA = %.3f ($given / $total), P = %.5f ($correct / $given), R = 
 print "Legenda: A = attempted, P = precision, R = recall, F1 = F1 measure\n";"""
 
 
-def fileids():
-    return  {'train': ['masaccio', 'computer_programming', 
-                       'wsj_0105_mrg', 'wsj_0186_mrg', 'wsj_0239_mrg', 
-                       'coarse_all_words', 'readme', 'sample_answer_file'],
-             'test': ['sense_cluster_21_senses','fs_baseline_key', 
-                      'dataset_21_test_key', 'scorer_pl']}
-    
-def sents(filename):
-    for line in globals()[filename].split('\n'):
-        yield line
-
-def test_instances():
-    """
-    Returns the test instances from SemEval2007 Coarse-grain WSD task.
-    
-    USAGE:
-    >>> for inst, sent, doc in test_instances():
-    ...     print inst
-    ...     print inst.id, inst.lemma, inst,word
-    ...     break
-    instance(id=u'd001.s001.t001', lemma=u'editorial', word=u'editorial')
-    d001.s001.t001 editorial editorial
-    """
-    Instance = namedtuple('instance', 'id, lemma, word')
-    for text in bsoup(coarse_all_words).findAll('text'):
-        textid = text['id']
-        document = " ".join([remove_tags(i) for i in str(text).split('\n') 
-                             if remove_tags(i)])
-        for sent in text.findAll('sentence'):
-            sentence =  " ".join([remove_tags(i) for i in str(sent).split('\n') 
-                             if remove_tags(i)])
-            for instance in sent.findAll('instance'):
-                instid = instance['id']
-                lemma = instance['lemma']
-                word = instance.text
-                inst = Instance(instid, lemma, word)
-                yield inst, unicode(sentence), unicode(document) 
             
