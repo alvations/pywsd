fa0c99d1ecca1a8ad3d1b01203baf5b073322378
alvas <alvations@gmail.com>
2014-09-23 11:05:10 +0200

    added some utility functions in utils.py

---
+++ pywsd/utils.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python -*- coding: utf-8 -*-
+
+def remove_tags(text):
+  """ Removes <tags> in angled brackets from text. """
+  import re
+  tags = {i:" " for i in re.findall("(<[^>\n]*>)",text.strip())}
+  no_tag_text = reduce(lambda x, kv:x.replace(*kv), tags.iteritems(), text)
+  return " ".join(no_tag_text.split())
