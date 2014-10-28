d27462fc37a796e514f8652cc100b527c8a1171b
alvas <alvations@gmail.com>
2014-10-28 17:22:38 +0100

    edited for a better demo for the corpus API

--- test_corpus.py
+++ test_corpus.py
@@ -8,11 +8,7 @@ semcor = SemCorpus('pywsd/data/semcor3.0_naf')
 for filename, sentence in semcor:
     context_sent_str = [i.text for i in sentence]
     context_sent_pos = [i.term.pos for i in sentence]
-    
-    print context_sent_str
-    print context_sent_pos
-    print
-
+    print sentence
 
 ######################################################################
 
@@ -21,5 +17,5 @@ from pywsd.semeval import SemEval2007_Coarse_WSD
 
 semeval = SemEval2007_Coarse_WSD('pywsd/data/semeval2007_coarse_grain_wsd/')
 
-for i in semeval:
-    print i
+for sentence in semeval:
+    print sentence
