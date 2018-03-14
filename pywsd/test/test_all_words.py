6d6d107636c0d816f7a6d15c7f6d39f0601a8bfb
alvations <alvations@gmail.com>
2018-03-14 18:16:26 +0800

    simple test case

--- pywsd/test/test_all_words.py
+++ pywsd/test/test_all_words.py
@@ -4,6 +4,22 @@
 import logging
 import unittest
 
-class TestAllWords(unittest.TestCase):
+from pywsd.lesk import simple_lesk, original_lesk, adapted_lesk, cosine_lesk
+
+
+class TestHelloWorld(unittest.TestCase):
     def test_hello_word_len(self):
         self.assertEqual(len("Hello World"), 11)
+
+
+class TestLesk(unittest.TestCase):
+    def test_simple_lesk_default(self):
+        bank_sents = [('I went to the bank to deposit my money', 'depository_financial_institution.n.01')
+                      ('The river bank was full of dead fishes', 'bank.n.01']
+
+        plant_sents = [('The workers at the industrial plant were overworked', 'plant.n.01')
+                       ('The plant was no longer bearing flowers', 'plant.v.01')]
+        for sent, synset_name in bank_sents:
+            self.assertEqual(simple_lesk(sent,'bank').name(), synset_name)
+        for sent, synset_name in plant_sents:
+            self.assertEqual(simple_lesk(sent,'plant').name(), synset_name)
