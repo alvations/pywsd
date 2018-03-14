0575acf21d25e4cf04ec3a1b933caf5214fd7912
alvations <alvations@gmail.com>
2018-03-14 15:21:23 +0800

    up version

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ from distutils.core import setup
 
 setup(
     name='pywsd',
-    version='1.1.4',
-    packages=['pywsd',],
+    version='1.1.6',
+    packages=['pywsd'],
     description='Python WSD',
     long_description='Python Implementations of Word Sense Disambiguation (WSD) technologies',
     url = 'https://github.com/alvations/pywsd',
-    package_data={'pywsd': ['data/signatures/*.pkl']},
+    package_data={'pywsd': ['data/signatures/*.pkl',]},
     license="MIT",
     install_requires = ['nltk', 'numpy', 'pandas']
 )
